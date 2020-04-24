42ae68614c11ad5185dd49546f9e359ea798562f
ali <github@alihong.net>
2020-04-24 22:21:48 +0000

    is_palindrome_permutation

--- src/arrays_and_strings/mod.rs
+++ src/arrays_and_strings/mod.rs
@@ -15,6 +15,7 @@ fn is_unique(s: &str) -> bool {
 
 // assertion:
 // s1 is a permutation of s2 if they contain the same letters in the same quantities
+//
 // notes:
 // alternate implementation could leverage an array the size of the charset.
 fn is_permutation(s1: &str, s2: &str) -> bool {
@@ -45,6 +46,41 @@ fn is_permutation(s1: &str, s2: &str) -> bool {
     true
 }
 
+// assertion:
+// a string is a palindrome permutation if...
+// 1) it has an even number of characters and each character occurs an even number of times
+// 2) it has an odd number of characters and every character except one occurs an even number of
+//    times
+fn is_palindrome_permutation(s: &str) -> bool {
+    let stripped_s: Vec<char> = s.chars().filter(|c| !c.is_whitespace()).collect();
+    let mut char_map: HashMap<char, i8> = HashMap::new();
+    for c in &stripped_s {
+        if c.is_whitespace() {
+            continue
+        }
+        let count = char_map.entry(*c).or_insert(0);
+        *count += 1;
+    };
+
+    let num_odd_counts = char_map
+        .iter()
+        .filter(|(_, c)| is_odd(c))
+        .count();
+    if num_odd_counts == 0 {
+        return true;
+    } else if num_odd_counts > 1 {
+        return false;
+    } else if num_odd_counts == 1 {
+        return is_odd(&(stripped_s.len() as i8))
+    } else {
+        panic!("can't have negative count")
+    }
+}
+
+fn is_odd(&x: &i8) -> bool {
+    x % 2 != 0
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -64,5 +100,12 @@ mod test {
         assert!(!is_permutation("qwerty", "qwertyqwert"));
         assert!(!is_permutation("qwerty", "qwertt"));
         assert!(!is_permutation("qwerty", "asdfgh"));
+    }
+
+    #[test]
+    fn test_is_palindrome_permutation() {
+        assert!(is_palindrome_permutation("tacocat"));
+        assert!(is_palindrome_permutation("taco cat"));
+        assert!(!is_palindrome_permutation("tacodog"));
     }
 }
